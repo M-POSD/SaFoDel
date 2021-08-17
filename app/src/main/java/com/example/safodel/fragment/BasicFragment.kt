8cddf2fb49730dfc36479a617d4c05d7ba423b67
Hsuan-Yu-Shih <hshi0043@student.monash.edu>
2021-08-18 01:37:38 +1000

    test

--- app/src/main/java/com/example/safodel/fragment/BasicFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/BasicFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.example.safodel.R
