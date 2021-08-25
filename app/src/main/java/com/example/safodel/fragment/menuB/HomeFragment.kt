ca0a8b091ee4314c8cfd0606a65e5fba8e098938
Hsuan-Yu-Shih <hshi0043@student.monash.edu>
2021-08-26 01:33:47 +1000

    updated all we needed for iteration1 remove and hide the unnecessary functions

--- app/src/main/java/com/example/safodel/fragment/menuB/HomeFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/menuB/HomeFragment.kt
@@ -30,7 +30,7 @@ class HomeFragment : BasicFragment<FragmentHomeBinding>(FragmentHomeBinding::inf
         binding.epicCard12.editTextLeft.text = "Ride Safer"
         binding.epicCard12.editTextRight.text = "E-Bike Delivery"
         binding.epicCard34.editTextLeft.text = "Safety Gear"
-        binding.epicCard34.editTextRight.text = "Placeholder"
+//        binding.epicCard34.editTextRight.text = "Placeholder"
 
         binding.epicCard12.cardLeft.setOnClickListener() {
             recordPosition(0)
