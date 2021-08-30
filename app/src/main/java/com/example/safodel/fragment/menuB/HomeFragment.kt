e128b44aa4c52342486e5d1b2fb2fd7f1eaa55e0
Hsuan-Yu-Shih <hshi0043@student.monash.edu>
2021-08-30 17:09:47 +1000

    slight updated

--- app/src/main/java/com/example/safodel/fragment/menuB/HomeFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/menuB/HomeFragment.kt
@@ -140,9 +140,9 @@ class HomeFragment : BasicFragment<FragmentHomeBinding>(FragmentHomeBinding::inf
 
     private fun imagesDrivingAnimation() {
         var objectAnimator1: ObjectAnimator =
-            ObjectAnimator.ofFloat(binding.images, "translationX", 0f, 2 * (view?.width ?: 1500) / 3.toFloat())
+            ObjectAnimator.ofFloat(binding.images, "translationX", 0f, 4 * (view?.width ?: 1500) / 5.toFloat())
         var objectAnimator2: ObjectAnimator =
-            ObjectAnimator.ofFloat(binding.images, "translationX", -2 * (view?.width ?: 1500) / 3.toFloat(), 0f)
+            ObjectAnimator.ofFloat(binding.images, "translationX", -4 * (view?.width ?: 1500) / 5.toFloat(), 0f)
         objectAnimator1.duration = 1500
         objectAnimator2.duration = 1500
         val animatorSet = AnimatorSet()
