3609a9de5afc35f39c4a4060c105a078b52cfcbc
KaihuaiX <kxuu0025@student.monash.edu>
2021-08-31 21:28:40 +1000

    edit login page

--- app/src/main/java/com/example/safodel/ui/main/LoginActivity.kt
+++ app/src/main/java/com/example/safodel/ui/main/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.safodel.ui.main
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,12 +13,18 @@ import java.security.MessageDigest
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private var wrongCount = 0
+    private lateinit var toast: Toast
     private var sha = MessageDigest.getInstance("SHA-256")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val pref = getSharedPreferences("login", Context.MODE_PRIVATE)
+        toast = Toast.makeText(this,"message",Toast.LENGTH_SHORT)
+        pref.edit().putBoolean("enabled",true).apply()
         binding.login.setOnClickListener {
             val password = binding.passwordEdit.text.toString().toByteArray()
             val byte = BigInteger(1,sha.digest(password)).toString()
@@ -26,9 +33,23 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             } else {
-                Toast.makeText(this, "account or password is invalid",
-                    Toast.LENGTH_SHORT).show()
+                val editor = pref.edit()
+                wrongCount++
+                if(wrongCount < 7) {
+                    toast.setText("account or password is invalid")
+                    toast.show()
+                }
+                else if(wrongCount >= 7 && wrongCount < 10){
+                    toast.setText("Last " + (10-wrongCount) +" chances.")
+                    toast.show()
+                }
+                else if(wrongCount == 10){
+                    editor.putBoolean("enabled",false)
+                    editor.apply()
+                    binding.passwordEdit.isEnabled = false
+                }
             }
         }
     }
+
 }
