0deeb84d232a6cc6610cdb708d0c10f17340a151
KaihuaiX <kxuu0025@student.monash.edu>
2021-09-03 18:40:45 +1000

    edit app intro page

--- app/src/main/java/com/example/safodel/fragment/menuL/AppIntroFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/menuL/AppIntroFragment.kt
@@ -1,10 +1,13 @@
 package com.example.safodel.fragment.menuL
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cn.refactor.lib.colordialog.ColorDialog
+import cn.refactor.lib.colordialog.PromptDialog
 import com.example.safodel.databinding.FragmentAppIntroBinding
 import com.example.safodel.fragment.BasicFragment
 
@@ -16,13 +19,34 @@ class AppIntroFragment : BasicFragment<FragmentAppIntroBinding>(FragmentAppIntro
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAppIntroBinding.inflate(inflater,container,false)
-        binding.appIntro.text = "Topic:" + "\n" + "\n" +
-                "Educating Food Delivery Bike Riders\n" + "\n" + "\n" +
-                "Audience:" + "\n" + "\n" +
-                "Potential and already existing food delivery bike riders"
-        binding.appIntro.gravity = Gravity.CENTER
-        binding.appIntro.textSize = 16F
+
         val toolbar = binding.toolbar.root
+
+        binding.about.paintFlags  = Paint.UNDERLINE_TEXT_FLAG
+        binding.about.setOnClickListener {
+            PromptDialog(context).setDialogType(PromptDialog.DIALOG_TYPE_SUCCESS)
+                .setAnimationEnable(true)
+                .setTitleText("Topic")
+                .setContentText("Educating Food Delivery Bike Riders\n")
+                .setPositiveListener ("OK"){
+                    it.dismiss()
+                }.show()
+        }
+
+        binding.audience.paintFlags  = Paint.UNDERLINE_TEXT_FLAG
+        binding.audience.setOnClickListener {
+            PromptDialog(context).setDialogType(PromptDialog.DIALOG_TYPE_SUCCESS)
+                .setAnimationEnable(true)
+                .setTitleText("Audience")
+                .setContentText("Potential and already existing food delivery bike riders\n")
+                .setPositiveListener("OK") {
+                    it.dismiss()
+                }.show()
+        }
+
+
+
+
         setToolbarBasic(toolbar)
         return binding.root
     }
