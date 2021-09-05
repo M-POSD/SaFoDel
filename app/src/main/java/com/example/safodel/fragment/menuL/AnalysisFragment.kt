80b8846c7e58014789c0edb6abd566699a26456b
KaihuaiX <kxuu0025@student.monash.edu>
2021-09-05 23:08:34 +1000

    add spinner in analysis page

--- app/src/main/java/com/example/safodel/fragment/menuL/AnalysisFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/menuL/AnalysisFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner
 import com.example.safodel.databinding.FragmentAnalysisBinding
 import com.example.safodel.databinding.FragmentExamBinding
 import com.example.safodel.fragment.BasicFragment
+import com.example.safodel.model.SuburbList
 
 
 class AnalysisFragment : BasicFragment<FragmentAnalysisBinding>(FragmentAnalysisBinding::inflate) {
+    private lateinit var spProvince: SmartMaterialSpinner<Any>
+    private var suburbList = SuburbList.init()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,11 +25,23 @@ class AnalysisFragment : BasicFragment<FragmentAnalysisBinding>(FragmentAnalysis
         _binding = FragmentAnalysisBinding.inflate(inflater, container, false)
         val toolbar = binding.toolbar.root
         setToolbarBasic(toolbar)
+        initSpinner()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initSpinner() {
+        spProvince = binding.spinner
+        spProvince.item = suburbList
+        spProvince.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(requireContext(),suburbList[position], Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(adapterView: AdapterView<*>) {}
+        }
     }
 }
