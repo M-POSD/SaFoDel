eb3c44f92cda5caf6ea6901709f3ed98b6f9197e
Hsuan-Yu-Shih <hshi0043@student.monash.edu>
2021-09-06 06:31:48 +1000

    Update know more to mahara industry mentor page

--- app/src/main/java/com/example/safodel/fragment/menuL/DeveloperFragment.kt
+++ app/src/main/java/com/example/safodel/fragment/menuL/DeveloperFragment.kt
@@ -1,9 +1,15 @@
 package com.example.safodel.fragment.menuL
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.example.safodel.R
 import com.example.safodel.databinding.FragmentDeveloperBinding
 import com.example.safodel.fragment.BasicFragment
 
@@ -15,13 +21,15 @@ class DeveloperFragment : BasicFragment<FragmentDeveloperBinding>(FragmentDevelo
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDeveloperBinding.inflate(inflater,container,false)
-        binding.developerTeam.text = "Team 27"
-        binding.developerTeam.textSize = 25.0F
-        binding.developerName.text = "CJ" + "\n" +
-                "Francis" + "\n" +
-                "Hsuanyu" + "\n" +
-                "James" + "\n" +
-                "Suvansh"
+
+        binding.knowMore.paintFlags  = Paint.UNDERLINE_TEXT_FLAG
+        binding.knowMore.setOnClickListener {
+            val url = "https://bit.ly/ta27-safodel"
+            val internetAct = Intent(Intent.ACTION_VIEW)
+            internetAct.data = Uri.parse(url)
+            startActivity(internetAct)
+        }
+
         val toolbar = binding.toolbar.root
         setToolbarBasic(toolbar)
         return binding.root
